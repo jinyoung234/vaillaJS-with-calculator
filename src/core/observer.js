90bb4b7b303a5b2fa0ec44c7bfdb689d2e80dcf5
손진영 <sm099@naver.com>
2023-08-15 01:07:29 +0900

    feat: observable 함수 Proxy로 개선

--- src/core/observer.js
+++ src/core/observer.js
@@ -7,19 +7,20 @@ export const observe = (callbackFunction) => {
 };
 
 export const observable = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    let _value = obj[key];
-    const observers = new Set();
-    Object.defineProperty(obj, key, {
-      get: () => {
-        if (currentObserver) observers.add(currentObserver);
-        return _value;
-      },
-      set: (value) => {
-        _value = value;
-        observers.forEach((callbackFunction) => callbackFunction());
-      },
-    });
+  const observerMap = {};
+
+  return new Proxy(obj, {
+    get(target, name) {
+      observerMap[name] = observerMap[name] || new Set();
+      if (currentObserver) observerMap[name].add(currentObserver);
+      return target[name];
+    },
+    set(target, name, value) {
+      if (target[name] === value) return true;
+      if (JSON.stringify(target[name]) === JSON.stringify(value)) return true;
+      target[name] = value;
+      observerMap[name].forEach((fn) => fn());
+      return true;
+    },
   });
-  return obj;
 };
